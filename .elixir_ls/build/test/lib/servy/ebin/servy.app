28527aed09ae152c73b989beca2c562501de955f
Joseph Giralt <voidEngineer@Josephs-MacBook-Pro.local>
2019-11-11 10:53:10 -0500

    add basic framework - wip

---
+++ .elixir_ls/build/test/lib/servy/ebin/servy.app
@@ -0,0 +1,6 @@
+{application,servy,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"servy"},
+              {modules,['Elixir.Servy','Elixir.Servy.Handler']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ lib/servy/handler.ex
@@ -0,0 +1,60 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
+  end
+
+  def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | status: 200, resp_body: "teddy, Smokey, Paddington"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "no #{path} here"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+end
